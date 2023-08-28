7e675a6c48c40ac4f2e1b43c71b73600c7bd057b
Soumyadeep Saha <soumyadeepsaha2345@gmail.com>
2023-08-28 13:02:38 +0530

    sv

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const session = require('express-session');
 const request = require('request');
 const axios = require('axios');
 const express = require('express');
-
+const bodyParser = require('body-parser'); // Add the bodyParser middleware
+const jwt = require('jsonwebtoken'); // Add the jsonwebtoken library
 
 const app = express();
 const port = 3000;
 
-app.use(session({
-    secret: 'my_secret',
-    resave: false,
-    saveUninitialized: true
-}))
+// app.use(session({
+//     secret: 'my_secret',
+//     resave: false,
+//     saveUninitialized: true
+// }))
+// app.use(bodyParser.urlencoded({ extended: true })); // Use bodyParser middleware
+
 
 // Set up express routes
 app.get('/', (req,res)=>{
@@ -21,12 +24,12 @@ app.get('/', (req,res)=>{
 });
 
 app.get('/login', (req, res) => {
-    const codeUrl = 'https://accounts.zoho.com/oauth/v2/auth';
+    const codeUrl = 'https://accounts.zoho.in/oauth/v2/auth';
 
     const queryParams = new URLSearchParams({
         response_type: 'code',
         client_id: process.env.CLIENT_ID,
-        scope: 'ZohoCRM.users.ALL',
+        scope: 'ZohoCampaigns.contact.ALL,email,profile',
         access_type: 'offline',
         redirect_uri: process.env.REDIRECT_URI,
     })
@@ -37,55 +40,38 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/callback', async (req, res) => {
-    const tokenEndPoint = 'http://accounts.zoho.com/oauth/v2/token'
+    const tokenEndPoint = 'https://accounts.zoho.in/oauth/v2/token';
     const { code } = req.query;
 
-    const reqBody = {
-        grant_type: 'authorization_code',
-        code: code,
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        redirect_uri: process.env.REDIRECT_URI
-    }
+    // Construct the request body using URL-encoded format
+    const reqBody = new URLSearchParams();
+    reqBody.append('grant_type', 'authorization_code');
+    reqBody.append('code', code);
+    reqBody.append('client_id', process.env.CLIENT_ID);
+    reqBody.append('client_secret', process.env.CLIENT_SECRET);
+    reqBody.append('redirect_uri', process.env.REDIRECT_URI);
 
-    const option = {
+    // Define options for the POST request
+    const options = {
         method: 'POST',
-        uri: tokenEndPoint,
-        form: reqBody,
-        json: true
-    }
+        url: tokenEndPoint,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data: reqBody.toString(), // Convert the request body to a URL-encoded string
+    };
 
     try {
-
-        const response = await request(option);
-        req.session.accessToken = response.access_token;
-        req.session.refreshToken = response.refresh_token;
-
-        res.redirect('/user');
-    } catch(err) {
-        console.log(error);
-        res.status(500).send('Error retriving access token');
+        const response = await axios(options);
+        console.log(response.data)
+        res.status(200).send(jwt.decode(response.data.id_token))
+    } catch (error) {
+        res.status(500).send('Error retrieving access token');
     }
 });
-
-app.get('/user', async(req,res)=>{
-    const userEndPoint = 'http://accounts.zoho.com/oauth/v2/userInfo';
-
-    const options = {
-        headers: {
-            Authorization: `Bearer ${req.session.accessToken}`
-        },
-        json: true        
-    }
-
-    try{
-        const response = await request.get(userEndpoint, options);
-        res.send(response);
-    } catch(err) {
-        res.send('Error retrieving user info');
-    }
-});
-
+app.get("/user", (req, res)=>{
+    res.status(200).send("hello")
+})
 // Start the Express server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
